2f94aa6bdc13fd1405c0dfd46cdb1c6d2fddd4ad
m.torkashvand <mtithut86@gmail.com>
2020-09-15 10:05:00 +0430

    refactor Home

--- pages/index.js
+++ pages/index.js
@@ -5,39 +5,27 @@ import styles from '../styles/Home.module.css'
 import {isLoginSuccess} from "../redux/signup/reducer";
 import Head from "next/head";
 
-class App extends React.Component {
-
-  render() {
-    console.log("props", this.props.counter)
-    const {loginSuccess} = this.props
+function App (props) {
+    const {loginSuccess} = props
     return (
       <>
         <Head>
           <title>خانه</title>
         </Head>
         <div className={styles.container}>
-          {/*<button onClick={this.props.incrementCounter}>Increment</button>*/}
-          {/*<button onClick={this.props.decrementCounter}>Decrement</button>*/}
-          {/*<h1>{this.props.counter}</h1>*/}
           <h1>خانه</h1>
           {loginSuccess ? <h2 className={styles.welcome}>به پلازیو خوش آمدید</h2> :
             < h2>< Link href={'/signup'}>ورود</Link></h2>}
         </div>
       </>
     );
-  }
 }
 
 const mapStateToProps = state => ({
   loginSuccess: isLoginSuccess(state),
-  // counter: state.counter.value
 });
 
 
 const mapDispatchToProps = {
-  // incrementCounter: incrementCounter,
-  // decrementCounter: decrementCounter,
 };
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-// export default  App
